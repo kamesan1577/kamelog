8b089d6cb0f153690a974acf369eb515ed2df2ff
kamesan <kamesan1577@gmail.com>
2026-09-10 02:14:42 +0900

    Revert "feat: サイト全体にkamelogロゴを組み込む" (#67)
    This reverts commit 314e69d73cbfabca6bc8194b6359a73779699630.

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ import "./full-page-blog-editor.css";
 import "./detail-actions.css";
 import "./blog-toc.css";
 import "./brand-theme.css";
-import "./brand-logo.css";
 import "./mobile-nav-layout.css";
 import "./tweet-link-preview.css";
 import "./project-social-links.css";
