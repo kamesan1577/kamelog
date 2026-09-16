e70838f868aa7fa7ee4f4301e1e4fb11f6ef0680
kamesan <kamesan1577@users.noreply.github.com>
2026-09-17 07:23:01 +0900

    [Issue #99] Extract inline composer styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import "../components/design-system/patterns/PostPreview.css";
 import "../components/design-system/patterns/PostCard.css";
 import "../components/design-system/patterns/TagList.css";
 import "../components/design-system/patterns/TimelineToolbar.css";
+import "../components/design-system/patterns/InlineComposer.css";
 import "../styles/tokens.css";
 import "../components/design-system/patterns/MobileBlogEditor.css";
 import "../components/design-system/patterns/ComposerLayout.css";
